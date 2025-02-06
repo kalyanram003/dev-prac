a14cd7f7229604f1990972a136398807fbbd8218
kalyanram003 <kalyanram2053@gmail.com>
2025-02-06 18:46:59 +0530

    added greet

--- src/main/java/mypkg/DemoProj.java
+++ src/main/java/mypkg/DemoProj.java
@@ -2,12 +2,16 @@ package mypkg;
 
 public class DemoProj {
 	
-	public String greet(String name) {
+	public String Intro(String name) {
 		return "kalyan "+name;
+	}
+	public String greet(String name) {
+		return "Hello "+name;
 	}
 	
 	public static void main(String[] args) {
 		DemoProj ob1=new DemoProj();
-		System.out.println("I am "+ob1.greet("Ram"));
+		System.out.println("I am "+ob1.Intro("Ram"));
+		System.out.println("I am "+ob1.greet("Linkedin"));
 	}
 }
