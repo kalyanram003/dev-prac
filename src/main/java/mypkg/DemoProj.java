b3fc26d2b670ccde05d4a4c0b1cd90c35f58ce09
kalyanram003 <kalyanram2053@gmail.com>
2025-02-06 18:53:05 +0530

    added infor

--- src/main/java/mypkg/DemoProj.java
+++ src/main/java/mypkg/DemoProj.java
@@ -8,10 +8,14 @@ public class DemoProj {
 	public String greet(String name) {
 		return "Hello "+name;
 	}
-	
+	public String Infor(String name) {
+		return "Learning "+name;
+	}
 	public static void main(String[] args) {
 		DemoProj ob1=new DemoProj();
 		System.out.println("I am "+ob1.Intro("Ram"));
 		System.out.println(ob1.greet("Linkedin"));
+		System.out.println(ob1.Infor("Jenkins"));
+
 	}
 }
